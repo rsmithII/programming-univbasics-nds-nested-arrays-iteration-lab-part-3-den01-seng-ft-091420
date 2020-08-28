ace58f711c453b1745b900c5e117122bd301008a
Rodney Smith II <r_smith3@hotmail.com>
2020-08-28 03:35:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
   ["The", 4, "quick"],
   [-1, "brown", "fox", 30],
   ["studied", 101, 233, "Ruby"]
-],
+]
 mixed_data_2 = [
   ["Turning", "and", "turning", "in", "the", "widening", "gyre"],
   ["The", "falcon", "cannot", "hear", "the", "falconer;"],
