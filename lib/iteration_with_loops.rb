571e999e29bdc3aa290b1cf694a95d373d17b60e
Rodney Smith II <r_smith3@hotmail.com>
2020-08-28 03:34:23 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,17 +1,17 @@
 def join_nested_strings(src)
   count = 0
   new_array = []
-  mixed_data = [
+  mixed_data_1 = [
   ["The", 4, "quick"],
   [-1, "brown", "fox", 30],
   ["studied", 101, 233, "Ruby"]
 ]
   
   while count < src.length do
-   mixed_data[0] = ["The", "quick"]
-   mixed_data[1] = ["brown", "fox"]
-   mixed_data[2] = ["studied", "Ruby"]
-   new_array = mixed_data.join(" ")
+   mixed_data_1[0] = ["The", "quick"]
+   mixed_data_1[1] = ["brown", "fox"]
+   mixed_data_1[2] = ["studied", "Ruby"]
+   new_array = mixed_data_1.join(" ")
     count += 1
   end
   new_array
