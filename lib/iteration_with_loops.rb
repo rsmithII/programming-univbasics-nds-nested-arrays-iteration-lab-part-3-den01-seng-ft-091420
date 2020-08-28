5b074298f3c30f2b0f69466942ab6a717505603e
Rodney Smith II <r_smith3@hotmail.com>
2020-08-28 03:56:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -25,7 +25,7 @@ mixed_data_2 = [
     count += 1
   end
   
-    new_array(mixed_data_2)
+    new_array
 end
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
