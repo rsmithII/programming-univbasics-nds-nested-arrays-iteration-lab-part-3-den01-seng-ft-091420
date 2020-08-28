b831290b1781aea7cb408b6a4ca67a6493fdd5b5
Rodney Smith II <r_smith3@hotmail.com>
2020-08-28 02:53:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,11 @@
 def join_nested_strings(src)
-  mixed_data_1.join("")
+  mixed_data_1[0].join("")
 end
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
   
+  mixed_data_1 = [
+  ["The", 4, "quick"],
+  [-1, "brown", "fox", 30],
+  ["studied", 101, 233, "Ruby"]
+]
